c617bd688b018a597aa2f9883667290f634dd34d
Onkar Dherange <51067778+OnkarSD@users.noreply.github.com>
2020-09-19 18:19:05 +0530

    Update script.js

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,3 +1,12 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.33 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1.3");
+// tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1.2 });
+// tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1.2 }, "-=1.2");
+
+
 $(document).ready(function () {
     $('.menu-toggler').on('click', function(){
         $(this).toggleClass('open');
@@ -29,12 +38,6 @@ $(document).ready(function () {
     });
 });
 
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.33 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1.3");
-// tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1.2 });
-// tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1.2 }, "-=1.2");
 
 
